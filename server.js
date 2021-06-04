5cf1ac98ec5888d2f26cc93fe88dd1daf22fa756
jacobxthomas <jacobthomas0801@gmail.com>
2021-06-04 14:17:47 +0800

    fixed connection and created main menu

--- server.js
+++ server.js
@@ -1,13 +1,67 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
-const promisemysql = require("promise-mysql");
+
+const connectionProperties = {
+    host: "127.0.0.1",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "employee_DB"
+}
 
 const connection = mysql.createConnection(connectionProperties);
 
-connection.connect((err) => {
-    if (err) throw err;
+connection.connect(function (err) {
+    if (err) console.log("error");
 
-    console.log("EMPLOYEE MANAGEMENT SYSTEM");
+    console.log("~~~ EMPLOYEE MANAGEMENT SYSTEM ~~~");
     mainMenu();
 });
+
+function mainMenu() {
+    inquirer.prompt({
+        name: "menu",
+        type: "list",
+        message: "Main Menu",
+        choices: [
+            "View Departments",
+            "View Roles",
+            "View Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee Role"]
+    })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View all employees":
+                    viewAllEmp();
+                    break;
+
+                case "View all employees by department":
+                    viewAllEmpByDept();
+                    break;
+
+                case "View all employees by role":
+                    viewAllEmpByRole();
+                    break;
+
+                case "Add employee":
+                    addEmp();
+                    break;
+
+                case "Add department":
+                    addDept();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Update employee role":
+                    updateEmpRole();
+                    break;
+            }
+
+        }
+        )
+};
